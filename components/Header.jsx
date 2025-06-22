f12e1003837ea873aaa93b870435055e7877966b
Indrawan <indrawanismail0@gmail.com>
2025-06-22 16:02:45 +0800

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+        <nav className="nav-wrapper">
+            <img className="nav-logo" src="./images/globe.png" alt="globe logo" />
+            <span className="nav-text">My travel jurnal.</span>
+        </nav>
+        </header>
+    )
+}
